6a2a404f0c9966c31fa1838e3879daaae9029c83
Alan He <xhe22@berkeley.edu>
2019-06-27 15:49:32 -0700

    Fixed Java code to compile locally

--- 2019/06 - June/530.java
+++ 2019/06 - June/530.java
@@ -1,32 +1,64 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 import java.util.ArrayList;
-class Solution {
+
+class q530 {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
     public int getMinimumDifference(TreeNode root) {
         ArrayList<Integer> iot = new ArrayList<>();
         inOrder(root, iot);
-        
+
         int curMin = Integer.MAX_VALUE;
         for (int i = 0; i < iot.size() - 1; i++) {
             int diff = iot.get(i+1) - iot.get(i);
             curMin = Math.min(curMin, diff);
         }
-        
+
         return curMin;
     }
-    
+
     private void inOrder(TreeNode root, ArrayList<Integer> iot) {
         if (root == null) { return; }
-        
+
         inOrder(root.left, iot);
         iot.add(root.val);
         inOrder(root.right, iot);
     }
 }
+
+///**
+// * Definition for a binary tree node.
+// * public class TreeNode {
+// *     int val;
+// *     TreeNode left;
+// *     TreeNode right;
+// *     TreeNode(int x) { val = x; }
+// * }
+// */
+//import java.util.ArrayList;
+//class Solution {
+//    public int getMinimumDifference(TreeNode root) {
+//        ArrayList<Integer> iot = new ArrayList<>();
+//        inOrder(root, iot);
+//
+//        int curMin = Integer.MAX_VALUE;
+//        for (int i = 0; i < iot.size() - 1; i++) {
+//            int diff = iot.get(i+1) - iot.get(i);
+//            curMin = Math.min(curMin, diff);
+//        }
+//
+//        return curMin;
+//    }
+//
+//    private void inOrder(TreeNode root, ArrayList<Integer> iot) {
+//        if (root == null) { return; }
+//
+//        inOrder(root.left, iot);
+//        iot.add(root.val);
+//        inOrder(root.right, iot);
+//    }
+//}
