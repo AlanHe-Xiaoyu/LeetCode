fe957a52c57589f6586d99d8bcaf24ef078fdc98
Xiaoyu HE (Alan) <xhe22@berkeley.edu>
2020-01-08 17:39:40 +0800

    Added soln to 176. First soln to 581

--- 2020/01 - Jan/176 Second_Highest_Salary.sql
+++ 2020/01 - Jan/176 Second_Highest_Salary.sql
@@ -10,3 +10,12 @@ SELECT
     ORDER BY Salary DESC
     LIMIT 1 OFFSET 1)
 AS SecondHighestSalary;
+
+
+"""
+Runtime: 318 ms, faster than 62.63% of MySQL online submissions for Second Highest Salary.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Second Highest Salary.
+"""
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee AS e
+WHERE e.Salary < (SELECT MAX(Salary) FROM Employee);
---
+++ 2020/01 - Jan/581 Shortest_Unsorted_Subarray.py
@@ -0,0 +1,25 @@
+class Solution:
+    # nums: List[int]
+    def findUnsortedSubarray(self, nums) -> int:
+
+        """
+        Soln 1
+        Runtime: 224 ms, faster than 50.00% of Python3 online submissions for Shortest Unsorted Continuous Subarray.
+        Memory Usage: 14 MB, less than 85.00% of Python3 online submissions for Shortest Unsorted Continuous Subarray.
+        """
+        sorted_nums = sorted(nums[:])
+        startingIdx = -1
+        for i in range(len(nums)):
+            if nums[i] != sorted_nums[i]:
+                startingIdx = i
+                break
+        
+        if startingIdx == -1:
+            return 0
+        
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i] != sorted_nums[i]:
+                endingIdx = i
+                break
+        
+        return endingIdx - startingIdx + 1
