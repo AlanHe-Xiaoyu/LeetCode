d9222e684bff4c922a7fca2d43d4c4209ac6b93c
Xiaoyu HE (Alan) <xhe22@berkeley.edu>
2020-01-08 12:55:22 +0800

    1290 done

---
+++ 2020/01 - Jan/1290 Convert_Binary_LL.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+
+        """
+        Runtime: 28 ms, faster than 66.59% of Python3 online submissions for Convert Binary Number in a Linked List to Integer.
+        Memory Usage: 12.6 MB, less than 100.00% of Python3 online submissions for Convert Binary Number in a Linked List to Integer.
+        """
+        val = 0
+        while head is not None:
+            val = val * 2 + head.val
+            head = head.next
+        return val
