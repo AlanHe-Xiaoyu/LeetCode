9ae2c96bc587379cd1cb609f9fdd1e6991c9b16a
Xiaoyu HE (Alan) <xhe22@berkeley.edu>
2020-01-23 14:43:52 -0800

    Q39 more solutions with better (and 1 worse) speeds

--- 2020/01 - Jan/39 Combination_Sum.py
+++ 2020/01 - Jan/39 Combination_Sum.py
@@ -25,3 +25,52 @@ class Solution:
         if result == [[]]:
             return []
         return result
+
+
+        """
+        Soln 2 - Similar to #1 but doesn't assume sortedness
+        Runtime: 412 ms, faster than 6.45% of Python3 online submissions for Combination Sum.
+        Memory Usage: 12.5 MB, less than 100.00% of Python3 online submissions for Combination Sum.
+        """
+        length = len(candidates)
+        candidates = sorted(candidates)
+        def helper(idx, remainder, cur):
+            nonlocal length
+            if remainder == 0:
+                return [cur]
+            elif idx >= length or remainder < 0:
+                return [[]]
+            
+            first_item = candidates[idx]
+            use_first = [lst for lst in helper(idx, remainder - first_item, cur + [first_item]) if lst != []]
+            no_use_first = [lst for lst in helper(idx + 1, remainder, cur) if lst != []]
+            
+            return use_first + no_use_first
+        
+        result = helper(0, target, [])
+        if result == [[]]:
+            return []
+        return result
+
+
+        """
+        Soln #3 - clean up #1 to reduce number of list comps used
+        Runtime: 40 ms, faster than 99.35% of Python3 online submissions for Combination Sum.
+        Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Combination Sum.
+        """
+        length = len(candidates)
+        candidates = sorted(candidates)
+        def helper(idx, remainder, cur):
+            nonlocal length
+            if remainder == 0:
+                return [cur]
+            elif idx >= length or remainder < candidates[idx]:
+                return [[]]
+            
+            first_item = candidates[idx]
+            use_first = helper(idx, remainder - first_item, cur + [first_item])
+            no_use_first = helper(idx + 1, remainder, cur)
+            
+            return use_first + no_use_first
+        
+        return [lst for lst in helper(0, target, []) if lst != []]
